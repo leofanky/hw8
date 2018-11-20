a88c8c78deae32fe75f200904e8bf3eaba9d01ec
leofanky <fillow99@gmail.com>
2018-11-20 01:43:51 +0100

    another commit

--- src/atm.java
+++ src/atm.java
@@ -1,139 +1,66 @@
-import javax.swing.JOptionPane;
-import java.util.Date;
-public class TestATM {
-   public static void main(String[] args) {
-   	    
-   //Creates 10 accounts
-      Account[] accounts = new Account[10];
-      for (int i = 0; i < accounts.length; i++) { 
-         accounts[i] = new Account(i, 100); 
-      }
-      
-      while(true) {
-      
-         boolean using = true;
-      
-         int idInput = Integer.parseInt(JOptionPane.showInputDialog("Enter your account ID"));
-         int index = -1; //holder position for the Account index
-      	
-         for(int i = 0; i < accounts.length; i++) {
-            if(accounts[i].getId() == idInput) {
-               index = i; //we have the index;
-               i = accounts.length;
-            }				
-         }
-         
-         if(index != -1) {
-            while(using) {
-            
-            //display menu 					
-               int choice = Integer.parseInt(JOptionPane.showInputDialog(null, 
-                    "      ATM main menu:\n" + "\n"
-                  + "1 - View account balance\n"
-                  + "2 - Withdraw funds\n"
-                  + "3 - Deposit funds\n"
-                  + "4 - Terminate transaction\n" + "\n"));
-                  
-               if (choice < 1 || choice > 4){
-                  JOptionPane.showMessageDialog(null, 
-                     "Invalid Choice" );
-               }
-                  
-               if (choice == 1){
-                  JOptionPane.showMessageDialog(null, 
-                     accounts[0].getDate() + "\n" + "\n" +
-                     "You have a balance of $" + accounts[index].getBalance() + "\n" + "\n");
-               
-               }
-               
-               if(choice == 2) {
-                  double amount = Double.parseDouble(JOptionPane.showInputDialog("Input withdraw amount!"));
-                  if(amount > accounts[index].getBalance()) {
-                     JOptionPane.showMessageDialog(null, "Insufficient Funds");
-                  } 
-                  else {
-                     accounts[index].withdraw(amount);
-                     JOptionPane.showMessageDialog(null, 
-                        "Withdrawal amount: $" + amount + "\n" + "New balance: $" + accounts[index].getBalance());
-                  }
-               }
-               
-               if(choice == 3) {
-                  double amount = Double.parseDouble(JOptionPane.showInputDialog("Input deposit amount!"));
-                  accounts[index].deposit(amount);
-                  JOptionPane.showMessageDialog(null, 
-                     "Deposit amount: $" + amount + "\n" + "New balance: $" + accounts[index].getBalance());
-               }
-            
-               if (choice == 4){
-                  using = false;
-               }
-            }
-         }
-         
-         else {
-         
-            JOptionPane.showMessageDialog(null, "Invalid ID");
-         }
-      }   
-   }
-}  
+import java.util.*;
 
-class Account {
+public class ATM implements Iterable<Dispenser> {
 
-   private int id = 0;
-   private double balance = 0;
-   private double withdraw = 0;
-   private double deposit = 0;
-   private double amount = 0;
-   private Date date = new Date();
-   
-   public Account() {
-   }
-   
-   public Account(int id, double balance){
-   
-      this.id = id;
-      this.balance = balance;
-   }
-         
-   public int getId(){
-   
-      return this.id;
-   }
-	
-   public void setId(int newId) {
-     
-      id = newId;	
-   }
-	  
-   public double getBalance() {
-     
-      return this.balance;   
-   }		  
-  
-   public java.util.Date getDate() {
-   
-      return this.date;      
-   }
-	
-   public double getWithdraw() {
-   
-      return this.withdraw;   
-   }
-	
-   public void withdraw(double amount) {
-   
-      balance -= amount;   
-   }
-   
-   public double getDeposit() {
-   
-      return this.deposit;   
-   }
-	
-   public void deposit(double amount) {
-   
-      balance += amount;
-   }
+    private Dispenser[] currencyDispensers;
+
+    public ATM(List<Integer> denominations, String currency, int initialBills) {
+        currencyDispensers = new Dispenser[denominations.size()];
+        denominations.sort((a, b) -> b - a);
+        for (int i = 0; i < denominations.size(); ++i)
+            currencyDispensers[i] = new Dispenser(currency, denominations.get(i), initialBills);
+        for (int i = 0; i + 1 < denominations.size(); ++i)
+            currencyDispensers[i].setNextChain(currencyDispensers[i + 1]);
+    }
+
+    public void withdraw(int amount) {
+        currencyDispensers[0].dispense(amount);
+    }
+
+    public class CDIterator implements Iterator {
+        int position;
+
+        public CDIterator(int position) {
+            this.position = position;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return position < currencyDispensers.length;
+        }
+
+        @Override
+        public Object next() {
+            return currencyDispensers[position++];
+        }
+    }
+
+    public int getBalance() {
+        int balance = 0;
+        for (Dispenser cd: this) {
+            balance += cd.billsLeft * cd.value;
+        }
+        return balance;
+    }
+
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> denom = new ArrayList();
+        denom.add(5);
+        denom.add(10);
+        denom.add(20);
+        denom.add(50);
+        denom.add(100);
+        ATM atm = new ATM(denom, "€", 10);
+        atm.withdraw(820);
+
+        System.out.println(atm.getBalance());
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new CDIterator(0);
+    }
+
 }
